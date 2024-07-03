cb60705abad55f0fb2ed1a30a0f70f511019f9f0
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-02 22:04:33 -0300

    Adicionando a model Shelter

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/model/Shelter.java
@@ -0,0 +1,74 @@
+package com.temporintech.animalhaven.model;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_SHELTER")
+public class Shelter implements Serializable{
+    
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private int capacity;
+
+    public Shelter(UUID id, String name, String address, String phoneNumber, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.capacity = capacity;
+    }
+
+    // Getters and setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
