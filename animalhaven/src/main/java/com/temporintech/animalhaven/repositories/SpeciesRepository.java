fdaf4b08bda6ab66826ecbe85ba480a4bd0db6fb
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-04 10:14:35 -0300

    Criando SpeciesRepository

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/repositories/SpeciesRepository.java
@@ -0,0 +1,13 @@
+package com.temporintech.animalhaven.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.temporintech.animalhaven.model.SpeciesModel;
+
+@Repository
+public interface SpeciesRepository extends JpaRepository<SpeciesModel, UUID>{
+
+}
