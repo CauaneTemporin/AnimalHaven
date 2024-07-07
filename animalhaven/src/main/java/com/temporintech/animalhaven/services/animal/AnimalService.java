1044bcb6922c82ea79c6cf3c79fe77ca4d81bd03
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-07 15:42:43 -0300

    Adicionando a implementação da saveAnimal

--- animalhaven/src/main/java/com/temporintech/animalhaven/services/animal/AnimalService.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/services/animal/AnimalService.java
@@ -1,8 +1,18 @@
 package com.temporintech.animalhaven.services.animal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.temporintech.animalhaven.model.AnimalModel;
+import com.temporintech.animalhaven.repositories.AnimalRepository;
 
-public interface AnimalService {
+public class AnimalService {
+	
+	@Autowired
+	AnimalRepository animalRepository;
 
-	AnimalModel save(AnimalModel animalModel);
+	@Transactional(readOnly = true)
+	public AnimalModel save(AnimalModel animalModel) {
+		return animalRepository.save(animalModel);
+	}
 }
