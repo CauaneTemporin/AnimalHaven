157e9b1fab730893ae84f4cb12781106da58411e
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-07 14:16:04 -0300

    Adicionando o save para o AnimalService

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/services/animal/AnimalService.java
@@ -0,0 +1,8 @@
+package com.temporintech.animalhaven.services.animal;
+
+import com.temporintech.animalhaven.model.AnimalModel;
+
+public interface AnimalService {
+
+	AnimalModel save(AnimalModel animalModel);
+}
