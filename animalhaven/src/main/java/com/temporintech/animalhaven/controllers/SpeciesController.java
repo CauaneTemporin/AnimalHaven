44e33aaba28a1bf29d7608a5f3dc234acdb72460
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-08 20:25:15 -0300

    Adicionando o getAllSpecies

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/SpeciesController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/SpeciesController.java
@@ -1,9 +1,12 @@
 package com.temporintech.animalhaven.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +27,13 @@ public class SpeciesController {
 
 	@PostMapping
 	public ResponseEntity<SpeciesModel> saveSpecies(@RequestBody @Valid SpeciesRecordDTO dto) {
-	var model = new SpeciesModel();
-	BeanUtils.copyProperties(dto, model);
-	return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(model));
+		var model = new SpeciesModel();
+		BeanUtils.copyProperties(dto, model);
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(model));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<SpeciesModel>> getAllSpecies() {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
 	}
 }
