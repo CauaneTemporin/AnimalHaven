7730c5a0f5782a537fb71a4ce28aa34b49911803
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-08 20:15:50 -0300

    Adicionando o saveSpecies

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/SpeciesController.java
@@ -0,0 +1,31 @@
+package com.temporintech.animalhaven.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.temporintech.animalhaven.dtos.SpeciesRecordDTO;
+import com.temporintech.animalhaven.model.SpeciesModel;
+import com.temporintech.animalhaven.repositories.SpeciesRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/species")
+public class SpeciesController {
+
+	@Autowired
+	SpeciesRepository repository;
+
+	@PostMapping
+	public ResponseEntity<SpeciesModel> saveSpecies(@RequestBody @Valid SpeciesRecordDTO dto) {
+	var model = new SpeciesModel();
+	BeanUtils.copyProperties(dto, model);
+	return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(model));
+	}
+}
