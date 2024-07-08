aeaccef99d0810da1036ac87f3946f2f3cfd2469
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-08 20:25:42 -0300

    Adicionando o getOneSpecies

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/SpeciesController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/SpeciesController.java
@@ -1,12 +1,15 @@
 package com.temporintech.animalhaven.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +38,14 @@ public class SpeciesController {
 	@GetMapping
 	public ResponseEntity<List<SpeciesModel>> getAllSpecies() {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getOneSpecies(@PathVariable(value = "id") UUID id) {
+		Optional<SpeciesModel> species = repository.findById(id);
+		if (species.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Species not found");
+
+		return ResponseEntity.status(HttpStatus.OK).body(species.get());
 	}
 }
