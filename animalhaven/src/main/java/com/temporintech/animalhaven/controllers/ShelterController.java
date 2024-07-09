f7a6094956f009bb6fa4eb514346d689d3c6c4c9
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-09 11:53:00 -0300

    Adicionando o updateShelter

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
@@ -7,11 +7,11 @@ import java.util.UUID;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,10 +44,21 @@ public class ShelterController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Object> getOneShelter(@PathVariable(value = "id") UUID id) {
-		Optional<ShelterModel> animal = repository.findById(id);
-		if (animal.isEmpty()) 
-			return ResponseEntity.status(HttpStatus.OK).body(animal.get());
-		
-		return ResponseEntity.status(HttpStatus.OK).body(animal.get());
+		Optional<ShelterModel> shelter = repository.findById(id);
+		if (shelter.isEmpty())
+			return ResponseEntity.status(HttpStatus.OK).body(shelter.get());
+
+		return ResponseEntity.status(HttpStatus.OK).body(shelter.get());
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateShelter(@PathVariable(value = "id") UUID id,
+			@RequestBody @Valid ShelterRecordDTO dto) {
+		Optional<ShelterModel> shelter = repository.findById(id);
+		if (shelter.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Shelter not found");
+		var shelterModel = shelter.get();
+		BeanUtils.copyProperties(dto, shelterModel);
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(shelterModel));
 	}
 }
