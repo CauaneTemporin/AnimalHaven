0238fef85b9f900856d478bc6e7f98f9b7323257
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-09 12:00:52 -0300

    Adicionando o deleteShelter

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +62,15 @@ public class ShelterController {
 		var shelterModel = shelter.get();
 		BeanUtils.copyProperties(dto, shelterModel);
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(shelterModel));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteShelter(@PathVariable(value = "id") UUID id) {
+		Optional<ShelterModel> shelter = repository.findById(id);
+
+		if (shelter.isEmpty())
+			return ResponseEntity.status(HttpStatus.OK).body("Shelter not found");
+		repository.delete(shelter.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Shelter deleted successfully");
 	}
 }
