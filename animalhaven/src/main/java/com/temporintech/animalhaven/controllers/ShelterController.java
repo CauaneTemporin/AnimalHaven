653cc7947ba56863ea0dec0d4dd4c4b0f1a8cae2
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-09 11:10:40 -0300

    Adicionando o saveShelter

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
@@ -1,5 +1,31 @@
 package com.temporintech.animalhaven.controllers;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.temporintech.animalhaven.dtos.ShelterRecordDTO;
+import com.temporintech.animalhaven.model.ShelterModel;
+import com.temporintech.animalhaven.repositories.ShelterRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/shelter")
 public class ShelterController {
 
+	@Autowired
+	ShelterRepository repository;
+
+	@PostMapping
+	public ResponseEntity<ShelterModel> saveShelter(@RequestBody @Valid ShelterRecordDTO dto) {
+		var model = new ShelterModel();
+		BeanUtils.copyProperties(dto, model);
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(model));
+	}
 }
