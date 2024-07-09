0482d6de0a9ed43285dc4decb59ad50ac28715b5
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-09 11:19:59 -0300

    Adicionando o getAllShelter

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
@@ -1,9 +1,12 @@
 package com.temporintech.animalhaven.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,10 @@ public class ShelterController {
 		var model = new ShelterModel();
 		BeanUtils.copyProperties(dto, model);
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(model));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ShelterModel>> getAllShelter() {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
 	}
 }
