39547973b66b7b978e02964b9f42710e3092b2b7
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-09 13:08:30 -0300

    Adicionando o updateVaccine

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/VaccineController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/VaccineController.java
@@ -11,10 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.temporintech.animalhaven.dtos.VaccineRecordDTO;
 import com.temporintech.animalhaven.model.VaccineModel;
 import com.temporintech.animalhaven.repositories.VaccineRepository;
 
@@ -47,4 +49,18 @@ public class VaccineController {
 
 		return ResponseEntity.status(HttpStatus.OK).body(vaccine.get());
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateVaccine(@PathVariable(value = "id") UUID id,
+			@RequestBody @Valid VaccineRecordDTO dto) {
+		Optional<VaccineModel> vaccine = repository.findById(id);
+		if (vaccine.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vaccine not found.");
+
+		var vaccineModel = vaccine.get();
+		BeanUtils.copyProperties(dto, vaccineModel);
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(vaccineModel));
+	}
+	
+
 }
