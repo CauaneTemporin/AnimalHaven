6b241bc70d77164a04b36a6a35af487341ba9948
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-09 11:27:58 -0300

    Adicionando o getOneShelter

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/ShelterController.java
@@ -1,18 +1,23 @@
 package com.temporintech.animalhaven.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.temporintech.animalhaven.dtos.ShelterRecordDTO;
+import com.temporintech.animalhaven.model.AnimalModel;
 import com.temporintech.animalhaven.model.ShelterModel;
 import com.temporintech.animalhaven.repositories.ShelterRepository;
 
@@ -35,5 +40,14 @@ public class ShelterController {
 	@GetMapping
 	public ResponseEntity<List<ShelterModel>> getAllShelter() {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getOneShelter(@PathVariable(value = "id") UUID id) {
+		Optional<ShelterModel> animal = repository.findById(id);
+		if (animal.isEmpty()) 
+			return ResponseEntity.status(HttpStatus.OK).body(animal.get());
+		
+		return ResponseEntity.status(HttpStatus.OK).body(animal.get());
 	}
 }
