2755a88fa864e9f910cf5ec9b47b48f1f3973237
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-09 12:55:13 -0300

    Adicionando o saveVaccine

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/VaccineController.java
@@ -0,0 +1,30 @@
+package com.temporintech.animalhaven.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.temporintech.animalhaven.model.VaccineModel;
+import com.temporintech.animalhaven.repositories.VaccineRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/vaccine")
+public class VaccineController {
+
+	@Autowired
+	VaccineRepository repository;
+
+	@PostMapping
+	public ResponseEntity<VaccineModel> saveVaccine(@RequestBody @Valid VaccineModel dto) {
+		var model = new VaccineModel();
+		BeanUtils.copyProperties(dto, model);
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(model));
+	}
+}
