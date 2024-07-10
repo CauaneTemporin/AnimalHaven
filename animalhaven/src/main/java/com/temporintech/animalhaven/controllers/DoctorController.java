8e1e22da25aff5f60ae5e1d336311dfe52275809
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-10 05:57:37 -0300

    Adicionando o deleteDoctor

--- animalhaven/src/main/java/com/temporintech/animalhaven/controllers/DoctorController.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/DoctorController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,4 +60,14 @@ public class DoctorController {
 		BeanUtils.copyProperties(dto, animalModel);
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(animalModel));
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteDoctor(@PathVariable(value = "id") UUID id) {
+		Optional<DoctorModel> doctor = repository.findById(id);
+		if (doctor.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Doctor not found.");
+		repository.delete(doctor.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Doctor deleted successfully.");
+	}
+s
 }
