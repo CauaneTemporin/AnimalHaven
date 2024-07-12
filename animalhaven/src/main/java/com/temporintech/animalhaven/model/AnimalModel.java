b283f1bb6ed213e748ea8b8feeb4f34cac9ab1ac
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-12 06:39:58 -0300

    Adicionando espécie para Animal

--- animalhaven/src/main/java/com/temporintech/animalhaven/model/AnimalModel.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/model/AnimalModel.java
@@ -15,97 +15,130 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "TB_ANIMAL")
-public class AnimalModel implements Serializable{
+public class AnimalModel implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
-    @Column(nullable = false)
+	@Column(nullable = false)
 	private String name;
-    @Column(nullable = false)
+	@Column(nullable = false)
 	private int age;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private Gender gender;
-    @Column(nullable = false)
+	@Column(nullable = false)
 	private double weight;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private Health health;
-    @Column(nullable = false)
+	@Column(nullable = false)
 	private Date dateEntered;
-    @Column(nullable = false)
+	@Column(nullable = false)
 	private String description;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private Status status;
-    @Column(nullable = false)
+	@Column(nullable = false)
 	private boolean castrated;
-    
+
+	@ManyToOne
+	@JoinColumn(name = "species_id", nullable = false)
+	private SpeciesModel species;
+
 	public UUID getId() {
 		return id;
 	}
+
 	public void setId(UUID id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public Gender getGender() {
 		return gender;
 	}
+
 	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
+
 	public double getWeight() {
 		return weight;
 	}
+
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
+
 	public Health getHealth() {
 		return health;
 	}
+
 	public void setHealth(Health health) {
 		this.health = health;
 	}
+
 	public Date getDateEntered() {
 		return dateEntered;
 	}
+
 	public void setDateEntered(Date dateEntered) {
 		this.dateEntered = dateEntered;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Status getStatus() {
 		return status;
 	}
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
+
 	public boolean isCastrated() {
 		return castrated;
 	}
+
 	public void setCastrated(boolean castrated) {
 		this.castrated = castrated;
+	}
+
+	public SpeciesModel getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(SpeciesModel species) {
+		this.species = species;
 	}
 }
