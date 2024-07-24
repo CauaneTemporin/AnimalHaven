1db3603eda95639066e5258c6a61b2554e89b978
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-24 11:29:35 -0300

    Adicionando o vaccineId como lista

--- animalhaven/src/main/java/com/temporintech/animalhaven/dtos/AnimalRecordDTO.java
+++ animalhaven/src/main/java/com/temporintech/animalhaven/dtos/AnimalRecordDTO.java
@@ -1,6 +1,7 @@
 package com.temporintech.animalhaven.dtos;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.UUID;
 
 import com.temporintech.animalhaven.enums.animal.Gender;
@@ -13,6 +14,6 @@ import jakarta.validation.constraints.Past;
 
 public record AnimalRecordDTO(@NotBlank String name, @NotNull int age, Gender gender, @NotNull double weight,
 		Health health, @Past Date dateEntered, @NotBlank String description, Status status, boolean castrated,
-		UUID speciesId, UUID shelterId, UUID cardId) {
+		UUID speciesId, UUID shelterId,  List<UUID> vaccineId) {
 
 }
