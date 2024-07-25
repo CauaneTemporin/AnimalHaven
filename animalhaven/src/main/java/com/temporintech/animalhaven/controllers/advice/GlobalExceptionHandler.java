54ebde09cb2de14d2acc0dd2c475f778f2a23168
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-25 10:32:13 -0300

    Capturar e formatar exceções retornar respostas HTTP apropriadas

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/controllers/advice/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.temporintech.animalhaven.controllers.advice;
+
+import com.temporintech.animalhaven.services.exceptions.ResourceNotFoundException;
+import com.temporintech.animalhaven.services.exceptions.AssociationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(AssociationException.class)
+    public ResponseEntity<String> handleAssociationException(AssociationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
+    }
+}
