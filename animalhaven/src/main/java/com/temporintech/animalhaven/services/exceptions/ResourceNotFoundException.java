9957969e11811d7867e8754bb13ebf8c7688387b
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-25 09:51:03 -0300

    Criação da ResourceNotFoundException para exibir a mensagem de erro

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.temporintech.animalhaven.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	public ResourceNotFoundException(String mensagem) {
+		super(mensagem);
+	}
+}
