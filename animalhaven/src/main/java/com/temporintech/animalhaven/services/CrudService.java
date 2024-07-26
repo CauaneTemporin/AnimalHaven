a69b828f6ac3dc9ba94c7bbf6245a854f442d886
CauaneTemporin <temporin.cauane@gmail.com>
2024-07-26 09:25:45 -0300

    Criação do CrudService

---
+++ animalhaven/src/main/java/com/temporintech/animalhaven/services/CrudService.java
@@ -0,0 +1,17 @@
+package com.temporintech.animalhaven.services;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CrudService<T, D> {
+	
+	T save(D dto);
+
+	T update(UUID id, D dto);
+
+	List<T> findAll();
+
+	T findById(UUID id);
+
+	void delete(UUID id);
+}
