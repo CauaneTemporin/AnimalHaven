a0796dcc2ea81390b75ff4b95fd21f029bc9b783
CauaneTemporin <temporin.cauane@gmail.com>
2024-09-11 06:02:18 -0300

    Base service Shelter

---
+++ animalhaven/src/test/java/com/temporintech/animalhaven/service/shelter/ShelterServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.temporintech.animalhaven.services.shelter;
+
+import com.temporintech.animalhaven.dtos.ShelterRecordDTO;
+import com.temporintech.animalhaven.model.ShelterModel;
+import com.temporintech.animalhaven.repositories.ShelterRepository;
+import com.temporintech.animalhaven.services.animal.AnimalService;
+import com.temporintech.animalhaven.services.exceptions.AssociationException;
+import com.temporintech.animalhaven.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ShelterServiceImplTest {
+
+    @Mock
+    private ShelterRepository repository;
+
+    @Mock
+    private AnimalService animalService;
+
+    @InjectMocks
+    private ShelterServiceImpl service;
+
+    private UUID shelterId;
+    private ShelterModel shelter;
+    private ShelterRecordDTO shelterDTO;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        shelterId = UUID.randomUUID();
+        shelter = new ShelterModel();
+        shelter.setId(shelterId);
+        shelter.setName("Shelter A");
+        shelter.setAddress("123 Street");
+        shelter.setPhoneNumber("123456789");
+        shelter.setCapacity(50);
+
+        shelterDTO = new ShelterRecordDTO("Shelter A", "123 Street", "123456789", 50);
+    }
+}
